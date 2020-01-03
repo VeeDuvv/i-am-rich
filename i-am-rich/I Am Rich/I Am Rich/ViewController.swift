53aafc8cbfb1ad03262a66edeb6a24ba57b68631
Vamsi Duvvuri <vamsi.duvvuri@icloud.com>
2020-01-03 13:03:19 -0500

    my version

---
+++ i-am-rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vamsi Duvvuri on 12/31/19.
+//  Copyright © 2019 Vamsi Duvvuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
